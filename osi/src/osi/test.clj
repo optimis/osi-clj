5b07efeaea9461525b70ce292d81b1683fc9f086
Dylan Jeffers <sapientech@openmailbox.org>
2016-12-31 18:00:43 -0800

    Add w-srvr convenience macro

---
+++ osi/src/osi/test.clj
@@ -0,0 +1,7 @@
+(ns osi.test
+  (:require [org.httpkit.server :refer [run-server]]))
+
+(defmacro w-srvr [app port body]
+  `(let [stop-srvr# (run-server ~app {:port ~port})]
+     ~body
+     (stop-srvr#)))
