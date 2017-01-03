dd167844d6a9cae2b715449898ca4d75ed730f4d
Umang Chouhan <uchouhan@optimisdev.com>
2017-01-03 11:24:03 -0800

    Bump ver.

--- osi/project.clj
+++ osi/project.clj
@@ -1,4 +1,4 @@
-(defproject osi "0.1.0-SNAPSHOT"
+(defproject osi "1.0.0-SNAPSHOT"
   :description "osi core library"
   :url "https://github.com/optimis"
   :license {:name "Eclipse Public License"
