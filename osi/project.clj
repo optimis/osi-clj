fba54a8d8b0c1077189d19329966158798c0ea97
Dylan Jeffers <sapientech@openmailbox.org>
2017-01-03 12:13:48 -0800

    Rm :repos

--- osi/project.clj
+++ osi/project.clj
@@ -3,13 +3,6 @@
   :url "https://github.com/optimis"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
-                                   :username [:env/dat_usr]
-                                   :password [:env/dat_psk]}
-                 "private" {:sign-releases false
-                            :url "s3p://osi-leiningen/releases/"
-                            :username :env/lein_repo_usr
-                            :passphrase :env/lein_repo_psk}}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [environ "1.1.0"]
                  [com.datomic/datomic-pro "0.9.5394" :exclusions [joda-time]]
