521c9eebfc0a23f7e89bafc0cabb7bdae7130e9a
Dylan Jeffers <sapientech@openmailbox.org>
2017-01-16 13:13:29 -0800

    Add schema utils

---
+++ osi/src/osi/http/schema.clj
@@ -0,0 +1,17 @@
+(ns osi.schema
+  (:require [schema.core :as s]
+            [schema.coerce :as coerce]
+            [clj-time.format :as f]
+            [clj-time.coerce :as c]))
+
+(def json-matchers {s/Inst (comp c/to-date f/parse)
+                    s/Num #(if (string? %)
+                             (float (read-string %))
+                             (float %))})
+
+(defn req-matcher [schema]
+  (or (coerce/json-coercion-matcher schema)
+      (+json-matchers schema)))
+
+(defn parse-req [schema req]
+  ((coerce/coercer schema req-matcher) req))
