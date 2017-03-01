c7c25f5d061ea3c1c08bbd196cd293b3ef610263
Umang Chouhan <uchouhan@optimisdev.com>
2017-02-28 23:19:06 -0800

    Add datomic test profile.

--- osi/profiles.clj
+++ osi/profiles.clj
@@ -1,6 +1,8 @@
 {:dev
  {:env {:name "development"
-        :uri "http://localhost:5678"}}
+        :uri "http://localhost:5678"
+        :datomic-db "test-db"
+        :datomic-storage "mem"}}
  :test
  {:env {:name "test"
         :uri "http://localhost:5678"}}}
