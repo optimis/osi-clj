44470f9f8e079ac040b7118558dd52c393ad2af4
Umang Chouhan <uchouhan@optimisdev.com>
2017-03-05 14:25:02 -0800

    Add dat prof.

--- osi/profiles.clj
+++ osi/profiles.clj
@@ -1,8 +1,12 @@
 {:dev
  {:env {:name "development"
         :uri "http://localhost:5678"
-        :datomic-db "test-db"
-        :datomic-storage "mem"}}
+        :datomic-db "test"
+        :datomic-storage "mem"
+        :datomic-storage-ip "127.0.0.1"}}
  :test
  {:env {:name "test"
+        :datomic-db "test"
+        :datomic-storage "mem"
+        :datomic-storage-ip "127.0.0.1"
         :uri "http://localhost:5678"}}}
