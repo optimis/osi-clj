de826de92fa00c386cdca4a04308203f6c32583d
Umang Chouhan <uchouhan@optimisdev.com>
2017-03-06 11:45:35 -0800

    Add UUID utils.

---
+++ osi/src/osi/util/uuid.clj
@@ -0,0 +1,14 @@
+(ns osi.util.uuid
+  (:import java.util.UUID))
+
+(defn- str->uuid [data]
+  (UUID/fromString
+   (replace data
+            #"(\w{8})(\w{4})(\w{4})(\w{4})(\w{12})"
+            "$1-$2-$3-$4-$5")))
+
+(defn bytes->uuid [bytes]
+  (str->uuid
+   (apply str
+          (for [b (take 16 bytes)]
+            (format "%02x" b)))))
