1b3da849e2411e8032ac5f785e76515508510166
Umang Chouhan <uchouhan@optimisdev.com>
2017-04-24 10:03:05 -0700

    Bump clj dep and tmp rm cloverage.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject osi-clj "0.4.0"
+(defproject osi-clj "0.9.0-SNAPSHOT"
   :description "osi clj library"
   :url "https://github.com/optimis"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]])
