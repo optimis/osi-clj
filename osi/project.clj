ac8425b5335034c0a9670d3f474f97e5c6d51af6
Umang Chouhan <uchouhan@optimisdev.com>
2017-10-22 23:17:04 -0700

    Upgrade clj to 1.9 beta2.

--- osi/project.clj
+++ osi/project.clj
@@ -10,7 +10,7 @@
                             :url "s3p://osi-leiningen/releases/"
                             :username :env/aws_access_key
                             :passphrase :env/aws_secret_key}}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha15"]
+  :dependencies [[org.clojure/clojure "1.9.0-beta2"]
                  [org.clojure/tools.logging "0.3.1"]
                  [org.clojure/math.combinatorics "0.0.7"]
                  [environ "1.1.0"]
