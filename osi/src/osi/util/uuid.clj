4191ccb1fac3ed82eef779e48b0f424aa9f75aee
Umang Chouhan <uchouhan@optimisdev.com>
2017-11-23 14:41:23 -0800

    Add fns to convert uuid to bytes.

--- osi/src/osi/util/uuid.clj
+++ osi/src/osi/util/uuid.clj
@@ -1,6 +1,8 @@
 (ns osi.util.uuid
   (:require [clojure.string :refer [replace]])
-  (:import java.util.UUID))
+  (:import
+   [java.nio ByteBuffer]
+   [java.util UUID]))
 
 (defn- str->uuid [data]
   (UUID/fromString
@@ -13,3 +15,18 @@
    (apply str
           (for [b (take 16 bytes)]
             (format "%02x" b)))))
+
+(defn bytes-to-uuid [^bytes b-a]
+  (let [byte-buffer (ByteBuffer/wrap b-a)
+        most-significant-bits (-> byte-buffer (.getLong 0))
+        least-significant-bits (-> byte-buffer (.getLong 8))]
+    (new UUID most-significant-bits least-significant-bits)))
+
+(defn uuid-to-bytes [uuid]
+  (-> (ByteBuffer/allocate 16)
+      (.putLong (.getMostSignificantBits uuid))
+      (.putLong (.getLeastSignificantBits uuid))
+      (.array)))
+
+(defn uuidstr->bytes [str]
+  (-> str UUID/fromString uuid-to-bytes))
