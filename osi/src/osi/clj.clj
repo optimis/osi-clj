7ddf5b4aa3125b2c3c532938ab1725d3c05f758e
Dylan Jeffers <sapientech@openmailbox.org>
2018-01-07 16:06:42 -0800

    Add find, deep-merge

--- osi/src/osi/clj.clj
+++ osi/src/osi/clj.clj
@@ -20,6 +20,8 @@
         (pred (first coll)) (first coll)
         :else (recur pred (rest coll))))
 
+(def find get-some)
+
 (defn split-every [n coll]
   (cond (empty? coll) coll
         (< (count coll) n) `(~coll)
@@ -31,3 +33,17 @@
    `(take ~num (drop ~offset ~coll)))
   ([coll num]
    `(limit ~coll ~num 0)))
+
+(defn- deep-merge* [& maps]
+  (let [f (fn [old new]
+            (if (and (map? old) (map? new))
+              (merge-with deep-merge* old new)
+              new))]
+    (if (every? map? maps)
+      (apply merge-with f maps)
+      (last maps))))
+
+(defn deep-merge [& maps]
+  (let [maps (filter identity maps)]
+    (assert (every? map? maps))
+    (apply merge-with deep-merge* maps)))
