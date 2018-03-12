76cb6648f9cd8ab7a21e49e37d7a48379ad0b105
kwakiekwak <john.bh.kwak@gmail.com>
2018-03-12 15:58:08 -0700

    Create ios file for universal links

---
+++ osi/src/osi/util/ios.clj
@@ -0,0 +1,10 @@
+(ns osi.util.ios)
+
+(defn apple-app-site-association [ios-prefix]
+  (let [appID (str ios-prefix ".com.osi.optimumMe")]
+    {:activitycontinuation
+     {:apps [appID]}
+     :applinks
+     {:apps []
+      :details: [{:appID appID
+                  :paths [*]}]}}))
