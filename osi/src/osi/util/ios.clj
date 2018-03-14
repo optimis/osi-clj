a785f0d9ea8e816a1d9a508e9e8f21dccdb7e264
kwakiekwak <john.bh.kwak@gmail.com>
2018-03-14 11:32:37 -0700

    Add existing app id to activitycontinuation and details

--- osi/src/osi/util/ios.clj
+++ osi/src/osi/util/ios.clj
@@ -1,8 +1,11 @@
 (ns osi.util.ios)
 
 (defn apple-app-site-association [ios-prefix]
-  (let [appID (str ios-prefix ".com.osi.optimumMe")]
-    {:activitycontinuation {:apps [appID]}
+  (let [appID (str ios-prefix ".com.osi.optimumMe")
+        oldAppId "75YPV78L8R.com.optimiscorp.optimumme.rc"]
+    {:activitycontinuation {:apps [appID oldAppId]}
      :applinks {:apps []
                 :details [{:appID appID
-                           :paths ["*"]}]}}))
+                           :paths ["/signup/*"]},
+                          {:appId oldAppId
+                           :paths ["/signup/*"]}]}}))
