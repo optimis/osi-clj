4bab60e02f3a7da8f57cb2bba972d6f655722798
Umang Chouhan <uchouhan@optimisdev.com>
2018-03-17 15:52:12 -0700

    Remove unused code.

--- osi/src/osi/http/client.clj
+++ osi/src/osi/http/client.clj
@@ -1,9 +1,7 @@
 (ns osi.http.client
-  (:require [environ.core :refer (env)]
-            [osi.http.util :refer [->transit <-transit ->json <-json content-type header]]
+  (:require [environ.core :refer [env]]
+            [osi.http.util :refer [->json <-json content-type header]]
             [org.httpkit.client :as http]))
-
-(def content-uri (env :content-uri))
 
 (defn- param [req p-map]
   (update-in req [:query-params] #(merge p-map %)))
@@ -29,11 +27,5 @@
 (defn parse-errs [resp]
   (if (errs? resp) (assoc resp :status 422)
       resp))
-
-(defn pull [req]
-  (http/post (str content-uri "/api") req))
-
-(defn pull-body [resp]
-  (-> @resp resp-body <-transit))
 
 
